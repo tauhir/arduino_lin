ce426d87b1ec38b8c6927999e7dce713a1af0f05
Tauhir Edwards <tauhir.edwards@gmail.com>
2021-08-05 16:42:04 +0200

    working on serial test to test master & slave

---
+++ master/master.ino
@@ -0,0 +1,24 @@
+#include "lin_stack.h"
+
+// LIN Object
+lin_stack LIN1(1);
+
+void setup() {
+  // Nothing to do here.
+}
+
+void loop() {
+  // Create Data Package
+  byte packageON[] = {255, 255}; // LEDs ON
+  LIN1.write(0x11, packageON, 2);
+  delay(5000);
+  byte packageOFF[] = {0, 0}; // LEDs OFF
+  LIN1.write(0x11, packageOFF, 2);
+  delay(5000);
+  	// 	LINBusSerialOne.begin(bound_rate); // config Serial
+		// LINBusSerialOne.write(0x55); // write Synch Byte to serial
+		// LINBusSerialOne.write(ident); // write Identification Byte to serial
+		// for(int i=0;i<data_size;i++) LINBusSerialOne.write(data[i]); // write data to serial
+		// LINBusSerialOne.write(checksum); // write Checksum Byte to serial
+		// LINBusSerialOne.end(); // clear Serial config
+}
