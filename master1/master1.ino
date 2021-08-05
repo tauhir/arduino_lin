926f4ebd2d8e6ab408d4f6df2c717153de44f9c1
Tauhir Edwards <tauhir.edwards@gmail.com>
2021-08-05 22:42:36 +0200

    serial test works

--- master1/master1.ino
+++ master1/master1.ino
@@ -21,30 +21,29 @@
 
 //PURE TEST -- uncomment below
 #include <AltSoftSerial.h>
-AltSoftSerial LINBus;
-
+AltSoftSerial LINBusSerialOne;
+byte packageON[] = {255, 255};
+byte packageOFF[] = {0, 0}; 
 
 void setup() {
   // Nothing to do here.
-  LINBus.begin(19200);
+  LINBusSerialOne.begin(19200);
 }
 //0x85 0x11 0x255 0x255 checksum
 // dummy checksum 0x23
 
 void loop() {
-  LINBus.write(0x55); // write Synch Byte to serial
+  LINBusSerialOne.write(0x55); // write Synch Byte to serial
   //should do microsend delay here
-  LINBus.write(0x11); // write Identification Byte to serial
-  LINBus.write(0xFF);
-  LINBus.write(0xFF);
-  LINBus.write(0x10); // write Checksum Byte to serial
+  LINBusSerialOne.write(0x11); // write Identification Byte to serial
+  for(int i=0;i<2;i++) LINBusSerialOne.write(packageON[i]); // write data to serial
+  LINBusSerialOne.write(0x23); // write Checksum Byte to serial
   delay(5000);
   
-  LINBus.write(0x55); // write Synch Byte to serial
+  LINBusSerialOne.write(0x55); // write Synch Byte to serial
   //should do microsend delay here
-  LINBus.write(0x11); // write Identification Byte to serial
-  LINBus.write(0x01);
-  LINBus.write(0x01);
-  LINBus.write(0x10); // write Checksum Byte to serial
+  LINBusSerialOne.write(0x11); // write Identification Byte to serial
+  for(int i=0;i<2;i++) LINBusSerialOne.write(packageOFF[i]); // write data to serial
+  LINBusSerialOne.write(0x23); // write Checksum Byte to serial
   delay(5000);
 }
--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -1,35 +1,23 @@
 #include <AltSoftSerial.h>
-// software serial #1: TX = digital pin 10, RX = digital pin 11
 AltSoftSerial portOne;
-int incomingByte[4];
+int incoming_byte = 0;
 
 void setup() {
-  // initialize serial:
   Serial.begin(19200);
-  // reserve 200 bytes for the inputString:
+  Serial.println("Start receiving");
   portOne.begin(19200);
   
 }
 
-
-//0x85 0x11 0x255 0x255 checksum
 void loop() {
-  // print the string when a newline arrives:
-  int counter = 0;
-  if (portOne.read() == 85){//sync byte
-    while ((portOne.available() > 0)&(counter<5)) {
-      incomingByte[counter] = portOne.read();
-      counter++;
+
+
+    while (portOne.available() > 0) {
+      incoming_byte = portOne.read();
+      if (incoming_byte == 85){
+        Serial.println(" ");
+      }
+      Serial.print(incoming_byte);
+      Serial.print(" ");
     }
-    Serial.println("I received: ");
-    for(int i=0;i<5;i++){
-        Serial.print(incomingByte[i], DEC);
-        Serial.print(" ");
-    }
-    Serial.println("");
-    Serial.println("done");
-  }
-  else {
-    return;
-  }
 }
